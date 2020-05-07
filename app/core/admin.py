d2cda3c7a76d9fda5c451541f330f22eae9d34a7
izadorozhnyi <izadorozhnyi@vibrenthealth.com>
2020-05-07 10:12:49 -0400

    typo

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
+from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
