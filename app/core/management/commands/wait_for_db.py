eeee1a9c93c3775aaa31f4f684c54d81e84208ea
izadorozhnyi <izadorozhnyi@vibrenthealth.com>
2020-05-07 16:32:00 -0400

    updated run command to include migrations and wait for db before standing up the server

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database not available, waiting 1 second...')
+                self.stdout.write('Db not available, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
